b3bf04f7a5c8316c803ce317e0a4b5a3d76a3a91
siskoweb <sisko4dev@gmail.com>
2024-09-25 15:51:08 +0100

    Refactore : change the way how to import dbOps

--- tests/e2e/auth.e2e.spec.js
+++ tests/e2e/auth.e2e.spec.js
@@ -1,65 +1,65 @@
-// const request = require('supertest');
-// const app = require('../../index');
-// const { userData } = require('../utils/authMockData');
+const request = require('supertest');
+const app = require('../../index');
+const { userData } = require('../utils/authMockData');
 
 
-// describe('User API Endpoints', () => {
-//     describe('User Registration', () => {
-//         it('should register a user successfully', async () => {
+describe('User API Endpoints', () => {
+    describe('User Registration', () => {
+        it('should register a user successfully', async () => {
 
-//             const response = await request(app)
-//                 .post('/api/v1/auth/register') // endpoint
-//                 .send(userData)
-//                 .set('Content-Type', 'application/json');
+            const response = await request(app)
+                .post('/api/v1/auth/register') // endpoint
+                .send(userData)
+                .set('Content-Type', 'application/json');
 
-//             expect(response.status).toBe(201); // expecting status
-//             expect(response.body).toHaveProperty('message', 'Created Account Successfully'); // Check the success message
-//             expect(response.body).toHaveProperty('data');
-//         });
+            expect(response.status).toBe(201); // expecting status
+            expect(response.body).toHaveProperty('message', 'Created Account Successfully'); // Check the success message
+            expect(response.body).toHaveProperty('data');
+        });
 
-//         it('should return an error for duplicate email', async () => {
-//             const userData = {
-//                 name: "yassine admin2",
-//                 email: "sisko1@gmail.com",
-//                 password: "pass123",
-//                 passwordConfirm: "pass123",
-//                 role: "user"
-//             };
+        it('should return an error for duplicate email', async () => {
+            const userData = {
+                name: "yassine admin2",
+                email: "sisko1@gmail.com",
+                password: "pass123",
+                passwordConfirm: "pass123",
+                role: "user"
+            };
 
-//             // first registration
-//             await request(app)
-//                 .post('/api/v1/auth/register')
-//                 .send(userData)
-//                 .set('Content-Type', 'application/json');
+            // first registration
+            await request(app)
+                .post('/api/v1/auth/register')
+                .send(userData)
+                .set('Content-Type', 'application/json');
 
-//             // second registration with the same email
-//             const response = await request(app)
-//                 .post('/api/v1/auth/register')
-//                 .send(userData)
-//                 .set('Content-Type', 'application/json');
+            // second registration with the same email
+            const response = await request(app)
+                .post('/api/v1/auth/register')
+                .send(userData)
+                .set('Content-Type', 'application/json');
 
-//             expect(response.status).toBe(400); // expection eror status 400
-//             // expect(response.body.error).toBe('Email already in use.'); // expecting error message
-//         });
+            expect(response.status).toBe(400); // expection eror status 400
+            // expect(response.body.error).toBe('Email already in use.'); // expecting error message
+        });
 
-//         it('should return an error for missing fields', async () => {
-//             const userData = {
-//                 name: "yassine admin2",
-//                 email: "sisko1@gmail.com",
-//                 password: "pass123",
-//                 // passwordConfirm is missing
-//                 role: "user"
-//             };
+        it('should return an error for missing fields', async () => {
+            const userData = {
+                name: "yassine admin2",
+                email: "sisko1@gmail.com",
+                password: "pass123",
+                // passwordConfirm is missing
+                role: "user"
+            };
 
-//             const response = await request(app)
-//                 .post('/api/v1/auth/register')
-//                 .send(userData)
-//                 .set('Content-Type', 'application/json');
+            const response = await request(app)
+                .post('/api/v1/auth/register')
+                .send(userData)
+                .set('Content-Type', 'application/json');
 
-//             expect(response.status).toBe(400);
+            expect(response.status).toBe(400);
 
-//             // expecting error message if passwordConfirm missing
-//             // expect(response.body.error).toBe('Password confirmation does not match.');
-//         });
-//     });
-// });
+            // expecting error message if passwordConfirm missing
+            // expect(response.body.error).toBe('Password confirmation does not match.');
+        });
+    });
+});
